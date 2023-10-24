0ef6078a812f9f798e50296e610325d1f22b695b
HUG0BA <127461683+HUG0BA@users.noreply.github.com>
2023-10-23 21:50:22 -0600

    Reorgazing Repo, copy of challenge done today

---
+++ Javascript/multiplication_tables.js
@@ -0,0 +1,47 @@
+//Soucre
+//Moure, B. (n.d.). Retos de programación semanales 2023. Retos de Programación. Retrieved October 24, 2023, from https://retosdeprogramacion.com/semanales2023
+/*
+ * Crea un programa que sea capaz de solicitarte un número y se
+ * encargue de imprimir su tabla de multiplicar entre el 1 y el 10.
+ * - Debe visualizarse qué operación se realiza y su resultado.
+ *   Ej: 1 x 1 = 1
+ *       1 x 2 = 2
+ *       1 x 3 = 3
+ *       ... 
+ */
+
+
+
+//------------------------FINAL SOLUTION---------------------------
+/*
+function mTables(number){
+    for(let i = 0; i <= 10; i++){
+        console.log(i + " X " + number + " = " + (i * number))
+    }
+}
+
+mTables(1)
+mTables(5)
+*/
+//--------------------------------------------------------------------
+
+
+//PROCESS AND EXPERIMENTS
+
+/*
+function mTables(number){
+    console.log("1" + (1 * number))
+}
+
+mTables(2) //Prints 12
+*/
+
+/*
+function mTables(number){
+    for(let i = 0; i <= 10; i++){
+        console.log(number + " X " + i + " = " + i * number)
+    }
+}
+
+mTables(19)
+*/
